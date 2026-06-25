ba76761da9db3d50c0f3b4890c8a2c19a49b6a30
Liya <liyastanly2@gmail.com>
2026-06-25 13:44:15 +0530

    Added Register page navigation to dashboard

--- backend/routes/task.js
+++ backend/routes/task.js
@@ -2,13 +2,44 @@ const express = require("express");
 
 const router = express.Router();
 
+const twilio = require("twilio");
+
+const client = twilio(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
+
 let tasks = [];
 
 // Add Task
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const task = req.body;
 
   tasks.push(task);
+
+  try {
+  await client.messages.create({
+    body: `🎓 CampusFlow Reminder
+
+Task: ${task.title}
+
+Subject: ${task.subject}
+
+Deadline: ${task.deadline}
+
+Please complete your task on time.
+
+- CampusFlow Team`,
+
+    from: process.env.TWILIO_WHATSAPP_NUMBER,
+
+    to: "whatsapp:+910000000000"
+  });
+
+  console.log("WhatsApp Message Sent Successfully");
+} catch (error) {
+  console.log(error);
+}
 
   console.log(tasks);
 
